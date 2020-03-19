007dfb549204d362ce84c11ffa7ff5d4c5eaaafd
Mitesh <mtsvasani@gmail.com>
2020-03-19 12:32:44 +0530

    added clear() function to clear the output screen after each round

--- akkad.py
+++ akkad.py
@@ -1,6 +1,13 @@
 ## game to  play akkad bakkad bambe bo
 
-import time, sys
+import time, sys, random
+from os import system, name
+
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
 
 players = []
 
@@ -13,8 +20,8 @@ for i in range(N):
     player = input(f'Player {i+1} :')
     players.append(player)
 
-# for player in players:
-#     print(player + '\t', end="")
+random.shuffle(players)
+
 
 rhyme = ['AKKAD', 'BAKKAD', 'BAMBE', 'BO', 'ASSI', 'NABBE', 'PURE', 'SAU', 'SAU', 'SE', 'NIKLA', 'DHAGA',
          'CHOR', 'NIKAL', 'KAR', 'BHAGA']
@@ -30,6 +37,7 @@ while(len(players) > 1):
         
         print("\n\n\n")
         time.sleep(.5)
+        clear()
 
     print(f"{players[i % len(players)]} eliminated !")
     
@@ -42,10 +50,11 @@ while(len(players) > 1):
         input('Press Enter to continue or Ctrl + C to quit')
     except KeyboardInterrupt:
         sys.exit()
+    clear()
 
 
 
 
 if len(players) == 1:
-    print(f"\nCongratulation {players[0]}, you are the winner!")  
+    print(f"\nCongratulation {players[0]}, you are the winner!\n\n")  
 
